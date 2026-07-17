3e93fe4cca1c2285a1100a9f25641ecbaa4eed57
ak <ajaytechworkshop@gmail.com>
2026-07-17 15:52:51 +0200

    - Restructured MQTT Publisher

--- mqtt/mqtt_publisher/src/mqtt_publisher/app.py
+++ mqtt/mqtt_publisher/src/mqtt_publisher/app.py
@@ -3,3 +3,4 @@ import paho.mqtt.client as mqtt_client
 client_id: str = "temp_sensor_publisher"
 mqtt_client = mqtt_client.Client(client_id=client_id, userdata=None, protocol=mqtt_client.MQTTv5)
 
+
